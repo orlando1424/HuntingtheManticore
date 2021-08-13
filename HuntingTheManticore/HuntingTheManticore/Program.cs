1c180da914e30bd4fd38e2b2245ac3eb7e6a5f81
Orlando <lopezorlando23@gmail.com>
2021-08-13 16:01:19 -0600

    init commit

---
+++ HuntingTheManticore/HuntingTheManticore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HuntingTheManticore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
